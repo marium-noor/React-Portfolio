d0c4f619bb1e7593d30e8542215fefc20afb1818
marium-noor <imariumnoor@gmail.com>
2024-06-11 01:04:06 +0500

    hi

--- src/components/HeroPortfolio.jsx
+++ src/components/HeroPortfolio.jsx
@@ -5,16 +5,16 @@ function HeroPortfolio() {
     <div className=' h-[80vh] flex justify-start ml-8 md:ml-20 items-center'>
       <div className='flex flex-col'>
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-10 w-5 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-16 w-8 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-10 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-16 w-12 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
               <span className="md:w-56 w-28 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
               <span className="md:w-48  w-24 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-24 w-12 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-10 w-5 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-24 w-16 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-8 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
