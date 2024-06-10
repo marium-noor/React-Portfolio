b1f0c0ea8ee8d1bedb0baced3e811af666ffd9d3
marium-noor <imariumnoor@gmail.com>
2024-06-11 01:09:23 +0500

    Resolved styling issues

--- src/components/HeroPortfolio.jsx
+++ src/components/HeroPortfolio.jsx
@@ -12,20 +12,20 @@ function HeroPortfolio() {
 
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-48  w-24 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-48  w-28 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
               <span className="md:w-24 w-16 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-10 w-8 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-6 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-24 w-12 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-10 w-5 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-48 w-24 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-24 w-16 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-6 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-48 w-28 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-10 w-5 bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
-              <span className="md:w-[300px] w-[150px] bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-[30px] bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-[300px] w-[170px] bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
 
@@ -35,27 +35,27 @@ function HeroPortfolio() {
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-10 w-5 bg-yellow-200 rounded-xl md:h-2  h-1 my-1"></span>
-              <span className="md:w-16 w-8  bg-yellow-200 rounded-xl md:h-2  h-1 my-1"></span>
-              <span className="md:w-56 w-28 bg-yellow-200 rounded-xl md:h-2  h-1 my-1"></span>
+              <span className="md:w-10 w-10 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-16 w-12 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-56 w-28 bg-yellow-200 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-48  w-24 bg-purple-300 rounded-xl md:h-2 h-1 my-1"></span>
-              <span className="md:w-24 w-12 bg-purple-300 rounded-xl md:h-2 h-1 my-1"></span>
-              <span className="md:w-10 w-5 bg-purple-300 rounded-xl md:h-2 h-1 my-1"></span>
+              <span className="md:w-48  w-28 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-24 w-16 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-6 bg-purple-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-24 w-12 bg-blue-300 rounded-xl md:h-2  h-1 my-1"></span>
-              <span className="md:w-10 w-5 bg-blue-300 rounded-xl md:h-2 h-1 my-1"></span>
-              <span className="md:w-48 w-24 bg-blue-300 rounded-xl md:h-2 h-1 my-1"></span>
+              <span className="md:w-24 w-16 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-10 w-6 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-48 w-28 bg-blue-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
-              <span className="md:w-10 w-5 bg-pink-300 rounded-xl md:h-2 h-1 my-1"></span>
-              <span className="md:w-[300px] w-[150px] bg-pink-300 rounded-xl md:h-2 h-1 my-1"></span>
+              <span className="md:w-10 w-[30px] bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
+              <span className="md:w-[300px] w-[170px] bg-pink-300 rounded-xl h-1 md:h-2 my-1"></span>
           </div>
       </div>
     </div>
