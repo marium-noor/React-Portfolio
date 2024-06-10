c3111048ef978bd201812d10490144938afe91a8
marium-noor <imariumnoor@gmail.com>
2024-06-11 01:01:55 +0500

    Changed hero styling

--- src/components/HeroPortfolio.jsx
+++ src/components/HeroPortfolio.jsx
@@ -30,8 +30,8 @@ function HeroPortfolio() {
 
 
           <div className='md:px-5 md:my-3 my-1 px-3'>
-              <h1 className='text-3xl md:text-5xl py-2 font-tiny5 text-slate-100'>Maryam...</h1>
-              <p className='uppercase text-sm md:text-xl font-semibold text-slate-400'>Frontend developer</p>
+              <h1 className='text-4xl md:text-5xl py-2 font-tiny5 text-slate-100'>Maryam...</h1>
+              <p className='uppercase text-lg md:text-xl font-semibold text-slate-400'>Frontend developer</p>
           </div>
 
           <div className='flex flex-row w-full space-x-2 md:space-x-3'>
